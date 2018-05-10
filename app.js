acea37a9ff84c97502bc6fd3230363d6d0ffcab2
rahul-radio <rahul.nowlakha@gmail.com>
2018-05-09 21:32:20 -0500

    Add to GitHub for versioning

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var port = parseInt(process.env.PORT) || 27017;
 
 // Tell express to listen to requests on a certain port
 //Need to add back process.env.PORT once ready to test.
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(27017, process.env.IP, function(){
     console.log("Server has started");
 });
 
