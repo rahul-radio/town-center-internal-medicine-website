ba15523df5a16dd3b6a14ae47d29e2422bd23040
Rahul Nowlakha <rahul@rahuls-mbp.lan>
2020-12-03 22:56:38 -0600

    Get rid of express ssl

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Require Node Modules
-var enforce = require("express-sslify");
+// var enforce = require("express-sslify");
 var bodyParser      = require("body-parser"),
     request         = require("request"),
     express         = require("express"),
